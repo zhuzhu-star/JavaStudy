c40d7cb6dd130880ed3164cc8a7dc6902e7c7688
zhubin <z707397b>
2020-09-18 12:06:02 +0800

    新增aop切面测试

---
+++ provider-zb/src/main/java/com/zb/ssm/spring/Aspect.java
@@ -0,0 +1,15 @@
+package com.zb.ssm.spring;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * @author ：zhubin
+ * @date ：Created in 2020/9/18 1:56 上午
+ * @description：这是一个切面aspect
+ * @modified By：
+ * @version: 1.0
+ */
+@EnableAspectJAutoProxy
+public class Aspect {
+
+}
